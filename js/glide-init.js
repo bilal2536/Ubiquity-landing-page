f964bd845906d38dc9c1c6f78d20b3eb19a13b3a
Bilal Jellouli <bilal.jellouli@gmail.com>
2021-04-29 23:45:42 +0200

    first commit

---
+++ js/glide-init.js
@@ -0,0 +1,17 @@
+new Glide('.hero .container', {
+    type: 'carousel',
+    gap: 0,
+    autoplay: 5000,
+    animationDuration: 1000,
+}).mount();
+
+new Glide('.testimonials .container', {
+    type: 'carousel',
+    gap: 0,
+    autoplay: 5000,
+    animationDuration: 1000
+}).mount();
+
+
+/* a11y = accessibility
+i18n = internationalization */
